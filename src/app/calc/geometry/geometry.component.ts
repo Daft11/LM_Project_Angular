af4a898df8fcac6973e3d69f99c5d8450e5dbb94
Vladislav_Eliseev <vlad_v96@mail.ru>
2021-01-19 02:29:06 +0700

    geomety component added - html css configured

--- src/app/calc/geometry/geometry.component.ts
+++ src/app/calc/geometry/geometry.component.ts
@@ -6,7 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./geometry.component.scss'],
 })
 export class GeometryComponent implements OnInit {
+  shape: string = 'line';
+  animToWall: string;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  getShape(): string {
+    return 'l-left';
+  }
+
+  animWall(inputId: number): void {
+    if (this.shape === 'line') this.animToWall = 'top';
+    if (this.shape === 'l-left') {
+      this.animToWall = inputId === 0 ? 'left' : 'top';
+    }
+    if (this.shape === 'l-right') {
+      this.animToWall = inputId === 0 ? 'top' : 'right';
+    }
+    this.animToWall = inputId === 0 ? 'left' : inputId === 1 ? 'top' : 'right';
+  }
 }
